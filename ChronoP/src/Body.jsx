42a5d62b9cb3943e61285da9b241799708198be3
ethfin <christophergalano13@gmail.com>
2025-02-07 18:35:08 +0800

    first commit

---
+++ ChronoP/src/Body.jsx
@@ -0,0 +1,10 @@
+function Body() {
+    return (
+        <div class="body">
+            <h1>Welcome to ChronoP</h1>
+            <p>This is a test Project</p>
+        </div>
+    );
+};
+
+export default Body;
---
+++ ChronoP/src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <h1>ChronoP</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Sign In</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
